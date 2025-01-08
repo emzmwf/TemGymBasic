8e64782dfa9e1b81531ad9bd5da54a2269d81f76
Michael Fay <52039648+emzmwf@users.noreply.github.com>
2025-01-08 12:24:15 +0000

    Update condenser_aperture_example_matplotlib.py
    Explicitly call temgymbasic in the imports

--- matplotlib_examples/condenser_aperture_example_matplotlib.py
+++ matplotlib_examples/condenser_aperture_example_matplotlib.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append(r"..")
-from components import Lens, Aperture
-from model import Model
+from temgymbasic.components import Lens, Aperture
+from temgymbasic.model import Model
 import numpy as np
-from run import show_matplotlib
+from temgymbasic.run import show_matplotlib
 
 components = [Aperture(name = 'Condenser Aperture', z = 0.3, x = 0, y = 0, aperture_radius_inner = 0.075),
               Lens(name = 'Condenser Lens', z = 0.2, f = -0.5)]
@@ -16,5 +16,3 @@ name = 'condenser_aperture.svg'
 fig, ax = show_matplotlib(model_, name = name)
 fig.suptitle('Condenser Aperture', fontsize=32)
 fig.savefig(name, dpi = 500)
-
-
